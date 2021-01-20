092375eeaefa356a735845d5a1df8c02cd4b9f0c
trimulyawan <77672842+trimulyawan@users.noreply.github.com>
2021-01-20 10:29:44 +0700

    Update pcie.c

--- hif/pcie/pcie.c
+++ hif/pcie/pcie.c
@@ -51,8 +51,8 @@ static struct mwl_chip_info pcie_chip_tbl[] = {
 		.fw_image	= "mwlwifi/88W8864.bin",
 		.cal_file	= NULL,
 		.txpwrlmt_file  = NULL,
-		.antenna_tx	= ANTENNA_TX_4_AUTO,
-		.antenna_rx	= ANTENNA_RX_4_AUTO,
+		.antenna_tx	= ANTENNA_TX_2_AUTO,
+		.antenna_rx	= ANTENNA_RX_2_AUTO,
 	},
 	[MWL8897] = {
 		.part_name	= "88W8897",
